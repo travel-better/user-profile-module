f9ac170dfc4285c7f0d7691396a745b332eb56c3
mutugiii <mutugimutuma@gmail.com>
2020-07-07 15:51:29 +0300

    User profile views

--- app/main/views.py
+++ app/main/views.py
@@ -3,42 +3,78 @@ from app import UserProfile
 from http import HTTPStatus
 from . import main
 
-@main.route('/userprofile', methods=['GET'])
+@main.route('/userprofile', methods=['GET', 'POST'])
 def get_profiles():
-    profiles = UserProfile.all()
-    response_data = []
-    for profile in profiles:
-        data = {
-            'id': profile.id,
-            'username': profile.username,
 
-        }
-        response_data.append(data)
-    return jsonify(response_data), HTTPStatus.OK
+    if request.method == 'GET':
+        profiles = UserProfile.all()
+        response_data = []
+        for profile in profiles:
+            data = {
+                'id': profile.id,
+                'username': profile.username,
+                'bio': profile.bio,
+                'total_reward_points': profile.total_reward_points,
+                'current_redeemable_points': profile.current_redeemable_points,
+                'user_total_carbon_footprint': profile.user_total_carbon_footprint,
+                'total_possible_carbon_footprint': profile.total_possible_carbon_footprint,
+                'total_carbon_footprint_reduced': profile.total_carbon_footprint_reduced,
+                'total_user_activities': profile.total_user_activities,
+            }
+            response_data.append(data)
+        return jsonify(response_data), HTTPStatus.OK
 
-@main.route('/userprofile', methods=['POST'])
-def create_profile():
-    request_data = request.get_json()
-    profile_data = dict(
-        username = request_data.get('username'),
+    if request.method == 'POST':
+        request_data = request.get_json()
+        profile_data = dict(
+            username = request_data.get('username'),
 
-    )
-    profile = UserProfile(**profile_data)
-    profile.store()
-    return HTTPStatus.CREATED
+        )
+        profile = UserProfile(**profile_data)
+        profile.store()
+        return HTTPStatus.CREATED
 
-@main.route('/userprofile/<string:profile_id>/', methods=['GET'])
-def retrieve_profile(profile_id):
+    return HTTPStatus.BAD_REQUEST
+
+@main.route('/userprofile/<int:profile_id>/', methods=['GET', 'PUT', 'DELETE'])
+def single_profile(profile_id):
     profile = UserProfile.load(id=profile_id)
     if not profile:
         return jsonify({
             'error': 'Profile not found.'
         }), HTTPStatus.BAD_REQUEST
     
-    response_data = {
-        'id': profile.id,
-        'username': profile.username,
+    if request.method == 'GET':
+        response_data = {
+            'id': profile.id,
+            'username': profile.username,
+            'bio': profile.bio,
+            'total_reward_points': profile.total_reward_points,
+            'current_redeemable_points': profile.current_redeemable_points,
+            'user_total_carbon_footprint': profile.user_total_carbon_footprint,
+            'total_possible_carbon_footprint': profile.total_possible_carbon_footprint,
+            'total_carbon_footprint_reduced': profile.total_carbon_footprint_reduced,
+            'total_user_activities': profile.total_user_activities,
+        }
 
-    }
+        return response_data, HTTPStatus.OK
 
-    return response_data, HTTPStatus.OK
+    if request.method == 'PUT':
+        request_data = request.get_json()
+        profile_data = dict(
+            username = request_data.get('username'),
+
+        )
+        profile = UserProfile(**profile_data)
+        profile.store()
+        return jsonify({
+            'message': 'Successfully Updated profile'
+        }), HTTPStatus.OK
+
+    if request.method == 'DELETE':
+        profile['_deleted'] = True
+        return jsonify({
+            'message': 'Successfully deleted profile'
+        }), HTTPStatus.OK
+
+    return HTTPStatus.BAD_REQUEST
