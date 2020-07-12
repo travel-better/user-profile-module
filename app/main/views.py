65aae62e1e87df6156b1cfa175bbfffa97fe55b7
mutugiii <mutugimutuma@gmail.com>
2020-07-12 22:05:40 +0300

    Refactor views code

--- app/main/views.py
+++ app/main/views.py
@@ -1,43 +1,35 @@
-from flask import jsonify, request
+from flask import jsonify, request, g
 from app import UserProfile
 from http import HTTPStatus
 from . import main
 
 @main.route('/userprofile', methods=['GET', 'POST'])
-def get_profiles():
-
+def profiles():
     if request.method == 'GET':
         profiles = UserProfile.all()
         response_data = []
-        for profile in profiles:
-            data = {
-                'id': profile.id,
-                'username': profile.username,
-                'bio': profile.bio,
-                'total_reward_points': profile.total_reward_points,
-                'current_redeemable_points': profile.current_redeemable_points,
-                'user_total_carbon_footprint': profile.user_total_carbon_footprint,
-                'total_possible_carbon_footprint': profile.total_possible_carbon_footprint,
-                'total_carbon_footprint_reduced': profile.total_carbon_footprint_reduced,
-                'total_user_activities': profile.total_user_activities,
-            }
-            response_data.append(data)
-        return jsonify(response_data), HTTPStatus.OK
+        try:
+            for profile in profiles.rows:
+                response_data.append(profile.__dict__)
+            return jsonify(response_data), HTTPStatus.OK
+        except couchdb.http.ResourceNotFound as e:
+            return jsonify({
+                'message': 'Ensure `all` view is correctly defined in the `userprofile` design document',
+                'error': e.args
+              }), HTTPStatus.BAD_REQUEST
 
     if request.method == 'POST':
-        request_data = request.get_json()
-        profile_data = dict(
-            username = request_data.get('username'),
-
-        )
-        profile = UserProfile(**profile_data)
+        profile = UserProfile(**request.get_json())
         profile.store()
-        return HTTPStatus.CREATED
+        return jsonify({
+            'message': 'Successfully added',
+            'id': profile.id
+            }), HTTPStatus.CREATED
 
     return HTTPStatus.BAD_REQUEST
 
-@main.route('/userprofile/<int:profile_id>/', methods=['GET', 'PUT', 'DELETE'])
-def single_profile(profile_id):
+@main.route('/userprofile/<string:profile_id>', methods=['GET', 'PUT', 'DELETE'])
+def profile(profile_id):
     profile = UserProfile.load(id=profile_id)
     if not profile:
         return jsonify({
@@ -45,34 +37,17 @@ def single_profile(profile_id):
         }), HTTPStatus.BAD_REQUEST
     
     if request.method == 'GET':
-        response_data = {
-            'id': profile.id,
-            'username': profile.username,
-            'bio': profile.bio,
-            'total_reward_points': profile.total_reward_points,
-            'current_redeemable_points': profile.current_redeemable_points,
-            'user_total_carbon_footprint': profile.user_total_carbon_footprint,
-            'total_possible_carbon_footprint': profile.total_possible_carbon_footprint,
-            'total_carbon_footprint_reduced': profile.total_carbon_footprint_reduced,
-            'total_user_activities': profile.total_user_activities,
-        }
-
-        return response_data, HTTPStatus.OK
+        return jsonify(profile.__dict__), HTTPStatus.OK
 
     if request.method == 'PUT':
-        request_data = request.get_json()
-        profile_data = dict(
-            username = request_data.get('username'),
-
-        )
-        profile = UserProfile(**profile_data)
+        profile = UserProfile(**request.get_json())
         profile.store()
         return jsonify({
             'message': 'Successfully Updated profile'
         }), HTTPStatus.OK
 
     if request.method == 'DELETE':
-        profile['_deleted'] = True
+        g.couch.delete(profile)
         return jsonify({
             'message': 'Successfully deleted profile'
         }), HTTPStatus.OK
