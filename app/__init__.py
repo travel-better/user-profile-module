6ef85252f65272e66989b1ec6386c75d5b2e614e
mutugiii <mutugimutuma@gmail.com>
2020-07-12 22:05:17 +0300

    App running

--- app/__init__.py
+++ app/__init__.py
@@ -2,7 +2,7 @@ from flask import Flask
 from config import config_options
 import flask_injector
 from flaskext.couchdb import CouchDBManager
-from .models import UserProfile
+from app.models import UserProfile
 
 INJECTOR_DEFAULT_MODULES = dict(
     db_manager=CouchDBManager,
@@ -14,17 +14,17 @@ def create_app(config_name, **kwargs):
     '''
     app = Flask(__name__)
 
-    # Initializing extensions
-    couch = CouchDBManager()
-    couch.add_document(UserProfile)
-    couch.setup(app)
-
     # Setting the config
     app.config.from_object(config_options[config_name])
 
     # Registering Blueprints
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint, url_prefix='/api/v1')
+
+    # Initializing couchdb manager
+    couch = CouchDBManager()
+    couch.add_document(UserProfile)
+    couch.setup(app)
 
     # Injecting db manager module
     flask_injector.FlaskInjector(app=app, modules=INJECTOR_DEFAULT_MODULES.values())
